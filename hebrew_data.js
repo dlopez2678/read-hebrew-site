64451db9076a960c2a7c516ce191119092e9304e
David Lopez <dlopez2678@gmail.com>
2024-08-17 21:22:44 -0400

    Added all Hebrew Letters

--- hebrew_data.js
+++ hebrew_data.js
@@ -10,6 +10,156 @@ export const flashCardLetters = [
     hLetterSymbol: '&#64305;',
     hLetterDiction: "B"
   },
+  {
+    hLetterName: "Vet",
+    hLetterSymbol: '&#1489;',
+    hLetterDiction: "V"
+  },
+  {
+    hLetterName: "Gimel",
+    hLetterSymbol: '&#1490;',
+    hLetterDiction: "G"
+  },
+  {
+    hLetterName: "Dalet",
+    hLetterSymbol: '&#1491;',
+    hLetterDiction: "D"
+  },
+  {
+    hLetterName: "Hey",
+    hLetterSymbol: '&#1492;',
+    hLetterDiction: "H"
+  },
+  {
+    hLetterName: "Vav",
+    hLetterSymbol: '&#1493;',
+    hLetterDiction: "V"
+  },
+  {
+    hLetterName: "Zayin",
+    hLetterSymbol: '&#1494;',
+    hLetterDiction: "Z"
+  },
+  {
+    hLetterName: "Chet",
+    hLetterSymbol: '&#1495;',
+    hLetterDiction: "CH"
+  },
+  {
+    hLetterName: "Tet",
+    hLetterSymbol: '&#1496;',
+    hLetterDiction: "T"
+  },
+  {
+    hLetterName: "Yud",
+    hLetterSymbol: '&#1497;',
+    hLetterDiction: "Y"
+  },
+  {
+    hLetterName: "Kaf",
+    hLetterSymbol: '&#64315;',
+    hLetterDiction: "K"
+  },
+  {
+    hLetterName: "Chaf",
+    hLetterSymbol: '&#1499;',
+    hLetterDiction: "CH"
+  },
+  {
+    hLetterName: "Final Chaf",
+    hLetterSymbol: '&#1498;',
+    hLetterDiction: "CH"
+  },
+  {
+    hLetterName: "Lamed",
+    hLetterSymbol: '&#1500;',
+    hLetterDiction: "L"
+  },
+  {
+    hLetterName: "Mem",
+    hLetterSymbol: '&#1502;',
+    hLetterDiction: "M"
+  },
+  {
+    hLetterName: "Final Mem",
+    hLetterSymbol: '&#1501;',
+    hLetterDiction: "M"
+  },
+  {
+    hLetterName: "Nun",
+    hLetterSymbol: '&#1504;',
+    hLetterDiction: "N"
+  },
+  {
+    hLetterName: "Final Nun",
+    hLetterSymbol: '&#1503;',
+    hLetterDiction: "N"
+  },
+  {
+    hLetterName: "Samech",
+    hLetterSymbol: '&#1505;',
+    hLetterDiction: "S"
+  },
+  {
+    hLetterName: "Ayin",
+    hLetterSymbol: '&#1506;',
+    hLetterDiction: "Silent"
+  },
+  {
+    hLetterName: "Pay",
+    hLetterSymbol: '&#64324;',
+    hLetterDiction: "P"
+  },
+  {
+    hLetterName: "Fay",
+    hLetterSymbol: '&#1508;',
+    hLetterDiction: "F"
+  },
+  {
+    hLetterName: "Final Fay",
+    hLetterSymbol: '&#1507;',
+    hLetterDiction: "F"
+  },
+  {
+    hLetterName: "Tsadi",
+    hLetterSymbol: '&#1510;',
+    hLetterDiction: "TZ"
+  },
+  {
+    hLetterName: "Final Tsadi",
+    hLetterSymbol: '&#1509;',
+    hLetterDiction: "TZ"
+  },
+  {
+    hLetterName: "Koof",
+    hLetterSymbol: '&#1511;',
+    hLetterDiction: "K"
+  },
+  {
+    hLetterName: "Resh",
+    hLetterSymbol: '&#1512;',
+    hLetterDiction: "R"
+  },
+  {
+    hLetterName: "Shin",
+    hLetterSymbol: '&#64298;',
+    hLetterDiction: "SH"
+  },
+  {
+    hLetterName: "Sin",
+    hLetterSymbol: '&#64299;',
+    hLetterDiction: "S"
+  },
+  {
+    hLetterName: "Taf",
+    hLetterSymbol: '&#64330;',
+    hLetterDiction: "TT"
+  },
+  {
+    hLetterName: "Taf",
+    hLetterSymbol: '&#1514;',
+    hLetterDiction: "T"
+  },
 ]
 
 export const flashCardVowels = [
